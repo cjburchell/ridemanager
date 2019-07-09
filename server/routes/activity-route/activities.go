2d3c95f2dec69e393b4d951b261d3dc479945dc5
Christiaan Burchell <christiaan.burchell@gemalto.com>
2019-07-09 15:02:45 -0400

    updated handling activites

--- server/routes/activity-route/activities.go
+++ server/routes/activity-route/activities.go
@@ -2,10 +2,9 @@ package activity_route
 
 import (
 	"encoding/json"
-	"net/http"
-
-	log "github.com/cjburchell/go-uatu"
+	"github.com/cjburchell/go-uatu"
 	"github.com/cjburchell/ridemanager/service/data/models"
+	"net/http"
 
 	"github.com/cjburchell/ridemanager/routes/token"
 	"github.com/cjburchell/ridemanager/service/data"
@@ -15,6 +14,22 @@ import (
 func Setup(r *mux.Router, service data.IService) {
 
 	dataRoute := r.PathPrefix("/api/v1/activity").Subrouter()
+
+	dataRoute.HandleFunc("/my", token.ValidateMiddleware(
+		func(writer http.ResponseWriter, request *http.Request) {
+			handleGetMyActivities(writer, request, service)
+		})).Methods("GET")
+
+	dataRoute.HandleFunc("/public", token.ValidateMiddleware(
+		func(writer http.ResponseWriter, request *http.Request) {
+			handleGetPublicActivities(writer, request, service)
+		})).Methods("GET")
+
+	dataRoute.HandleFunc("/joined", token.ValidateMiddleware(
+		func(writer http.ResponseWriter, request *http.Request) {
+			handleGetJoinedActivities(writer, request, service)
+		})).Methods("GET")
+
 	dataRoute.HandleFunc("/{ActivityId}", token.ValidateMiddleware(
 		func(writer http.ResponseWriter, request *http.Request) {
 			handleGetActivity(writer, request, service)
@@ -22,24 +37,58 @@ func Setup(r *mux.Router, service data.IService) {
 
 	dataRoute.HandleFunc("/", token.ValidateMiddleware(
 		func(writer http.ResponseWriter, request *http.Request) {
-			handleGetActivities(writer, request, service)
-		})).Methods("GET")
-
-	dataRoute.HandleFunc("/", token.ValidateMiddleware(
-		func(writer http.ResponseWriter, request *http.Request) {
 			handleCreateActivity(writer, request, service)
 		})).Methods("POST")
 
-	dataRoute.HandleFunc("/{ActivityId}", token.ValidateMiddleware(
+	dataRoute.HandleFunc("/{ActivityId}", token.ValidateMiddleware(validateWritableAccessMiddleware(
 		func(writer http.ResponseWriter, request *http.Request) {
 			handleUpdateActivity(writer, request, service)
-		})).Methods("PATCH")
+		}, service))).Methods("PATCH")
 
-	dataRoute.HandleFunc("/{ActivityId}", token.ValidateMiddleware(
+	dataRoute.HandleFunc("/{ActivityId}", token.ValidateMiddleware(validateWritableAccessMiddleware(
 		func(writer http.ResponseWriter, request *http.Request) {
 			handleDeleteActivity(writer, request, service)
-		})).Methods("DELETE")
+		}, service))).Methods("DELETE")
 
+}
+
+func validateWritableAccessMiddleware(next http.HandlerFunc, service data.IService) http.HandlerFunc {
+	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+		 user, err := token.GetUser(req, service)
+		 if err != nil{
+			 w.WriteHeader(http.StatusUnauthorized)
+			 return
+		 }
+
+		if user == nil{
+			w.WriteHeader(http.StatusUnauthorized)
+			return
+		}
+
+		// check user role
+		if user.Role == models.AdminRole{
+			next(w, req)
+			return
+		}
+
+		 // check activity owner
+		vars := mux.Vars(req)
+		activityId := models.ActivityId(vars["ActivityId"])
+
+		activity, err := service.GetActivity(activityId)
+		if err != nil {
+			log.Error(err)
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
+		if activity.OwnerId != user.Id{
+			w.WriteHeader(http.StatusUnauthorized)
+			return
+		}
+
+		next(w, req)
+	})
 }
 
 func handleDeleteActivity(writer http.ResponseWriter, request *http.Request, service data.IService) {
@@ -57,34 +106,130 @@ func handleDeleteActivity(writer http.ResponseWriter, request *http.Request, ser
 }
 
 func handleUpdateActivity(writer http.ResponseWriter, request *http.Request, service data.IService) {
+	decoder := json.NewDecoder(request.Body)
+	var activity models.Activity
+	err := decoder.Decode(&activity)
+	if err != nil {
+		log.Error(err)
+		writer.WriteHeader(http.StatusInternalServerError)
+		return
+	}
 
+	err = service.UpdateActivity(activity)
+	if err != nil {
+		log.Error(err)
+		writer.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	writer.WriteHeader(http.StatusOK)
 }
 
 func handleCreateActivity(writer http.ResponseWriter, request *http.Request, service data.IService) {
+	decoder := json.NewDecoder(request.Body)
+	var activity models.Activity
+	err := decoder.Decode(&activity)
+	if err != nil {
+		log.Error(err)
+		writer.WriteHeader(http.StatusInternalServerError)
+		return
+	}
 
+	err = service.AddActivity(&activity)
+	if err != nil {
+		log.Error(err)
+		writer.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	writer.WriteHeader(http.StatusOK)
 }
 
-func handleGetActivities(writer http.ResponseWriter, request *http.Request, service data.IService) {
+func handleGetMyActivities(writer http.ResponseWriter, req *http.Request, service data.IService) {
 
+	user, err := token.GetUser(req, service)
+	if err != nil{
+		writer.WriteHeader(http.StatusUnauthorized)
+		return
+	}
+
+	activities, err := service.GetOwnedActivities(user.Id)
+	if err != nil {
+		log.Error(err)
+		writer.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	reply, _ := json.Marshal(activities)
+	writer.Header().Set("Content-Type", "application/json")
+	writer.WriteHeader(http.StatusOK)
+
+	_, err = writer.Write(reply)
+	if err != nil {
+		log.Error(err)
+	}
 }
 
-func handleGetActivity(w http.ResponseWriter, r *http.Request, service data.IService) {
+func handleGetJoinedActivities(writer http.ResponseWriter, req *http.Request, service data.IService) {
 
-	vars := mux.Vars(r)
+	user, err := token.GetUser(req, service)
+	if err != nil{
+		writer.WriteHeader(http.StatusUnauthorized)
+		return
+	}
+
+	activities, err := service.GetAthleteActivities(user.Id)
+	if err != nil {
+		log.Error(err)
+		writer.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	reply, _ := json.Marshal(activities)
+	writer.Header().Set("Content-Type", "application/json")
+	writer.WriteHeader(http.StatusOK)
+
+	_, err = writer.Write(reply)
+	if err != nil {
+		log.Error(err)
+	}
+}
+
+func handleGetPublicActivities(writer http.ResponseWriter, _ *http.Request, service data.IService) {
+	activities, err := service.GetActivitiesByPrivacy(models.PublicActivity)
+	if err != nil {
+		log.Error(err)
+		writer.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	reply, _ := json.Marshal(activities)
+	writer.Header().Set("Content-Type", "application/json")
+	writer.WriteHeader(http.StatusOK)
+
+	_, err = writer.Write(reply)
+	if err != nil {
+		log.Error(err)
+	}
+}
+
+func handleGetActivity(writer http.ResponseWriter, request *http.Request, service data.IService) {
+
+	vars := mux.Vars(request)
 	activityId := models.ActivityId(vars["ActivityId"])
 
 	activity, err := service.GetActivity(activityId)
 	if err != nil {
 		log.Error(err)
-		w.WriteHeader(http.StatusInternalServerError)
+		writer.WriteHeader(http.StatusInternalServerError)
 		return
 	}
 
 	reply, _ := json.Marshal(activity)
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
+	writer.Header().Set("Content-Type", "application/json")
+	writer.WriteHeader(http.StatusOK)
 
-	_, err = w.Write(reply)
+	_, err = writer.Write(reply)
 	if err != nil {
 		log.Error(err)
 	}
