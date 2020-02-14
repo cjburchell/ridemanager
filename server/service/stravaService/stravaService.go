eee23a738e29bfb2a037c5af88e1b7c071f67375
Christiaan Burchell <christiaan.burchell@gemalto.com>
2020-02-14 16:51:34 -0500

    suff

--- server/service/stravaService/stravaService.go
+++ server/service/stravaService/stravaService.go
@@ -68,52 +68,17 @@ func (s service) GetRouteStreams(routeId int64) (*strava.StreamSet, error) {
 	return &result, err
 }
 
-func GetRouteStreams(a *strava.StreamsApiService, ctx context.Context, id int64) (strava.StreamSet, *http.Response, error) {
-	var (
-		localVarHttpMethod  = strings.ToUpper("Get")
-		localVarPostBody    interface{}
-		localVarFileName    string
-		localVarFileBytes   []byte
-		localVarReturnValue strava.StreamSet
-	)
-
+func  (s service)GetRouteStreams(a *strava.StreamsApiService, ctx context.Context, id int64) (strava.StreamSet, *http.Response, error) {
 	// create path and map variables
-	cfg := strava.NewConfiguration();
+	cfg := strava.NewConfiguration()
 	localVarPath := cfg.BasePath + "/routes/{id}/streams"
 	localVarPath = strings.Replace(localVarPath, "{"+"id"+"}", fmt.Sprintf("%v", id), -1)
 
-	localVarHeaderParams := make(map[string]string)
-	localVarQueryParams := url.Values{}
-	localVarFormParams := url.Values{}
 
-	http.
-
-	// to determine the Content-Type header
-	localVarHttpContentTypes := []string{}
-
-	// set Content-Type header
-	localVarHttpContentType := strava.selectHeaderContentType(localVarHttpContentTypes)
-	if localVarHttpContentType != "" {
-		localVarHeaderParams["Content-Type"] = localVarHttpContentType
-	}
-
-	// to determine the Accept header
-	localVarHttpHeaderAccepts := []string{"application/json"}
-
-	// set Accept header
-	localVarHttpHeaderAccept := strava.selectHeaderAccept(localVarHttpHeaderAccepts)
-	if localVarHttpHeaderAccept != "" {
-		localVarHeaderParams["Accept"] = localVarHttpHeaderAccept
-	}
-	r, err := a.client.prepareRequest(ctx, localVarPath, localVarHttpMethod, localVarPostBody, localVarHeaderParams, localVarQueryParams, localVarFormParams, localVarFileName, localVarFileBytes)
-	if err != nil {
-		return localVarReturnValue, nil, err
-	}
-
-	localVarHttpResponse, err := a.client.callAPI(r)
-	if err != nil || localVarHttpResponse == nil {
-		return localVarReturnValue, localVarHttpResponse, err
-	}
+	var localVarReturnValue strava.StreamSet
+	req, err := http.NewRequest("GET", localVarPath, nil);
+	req.Header.Add("Authorization", fmt.Sprintf("Bearer %s", ctx.Value(strava.ContextAccessToken)))
+	localVarHttpResponse, err := http.Client{}.Do(req)
 
 	localVarBody, err := ioutil.ReadAll(localVarHttpResponse.Body)
 	localVarHttpResponse.Body.Close()
@@ -130,34 +95,7 @@ func GetRouteStreams(a *strava.StreamsApiService, ctx context.Context, id int64)
 	}
 
 	if localVarHttpResponse.StatusCode >= 300 {
-		newErr := strava.GenericSwaggerError{
-			body:  localVarBody,
-			error: localVarHttpResponse.Status,
-		}
-
-		if localVarHttpResponse.StatusCode == 200 {
-			var v strava.StreamSet
-			err = a.client.decode(&v, localVarBody, localVarHttpResponse.Header.Get("Content-Type"))
-			if err != nil {
-				newErr.error = err.Error()
-				return localVarReturnValue, localVarHttpResponse, newErr
-			}
-			newErr.model = v
-			return localVarReturnValue, localVarHttpResponse, newErr
-		}
-
-		if localVarHttpResponse.StatusCode == 0 {
-			var v strava.Fault
-			err = a.client.decode(&v, localVarBody, localVarHttpResponse.Header.Get("Content-Type"))
-			if err != nil {
-				newErr.error = err.Error()
-				return localVarReturnValue, localVarHttpResponse, newErr
-			}
-			newErr.model = v
-			return localVarReturnValue, localVarHttpResponse, newErr
-		}
-
-		return localVarReturnValue, localVarHttpResponse, newErr
+		return localVarReturnValue, localVarHttpResponse, fmt.Errorf("bad Status Code")
 	}
 
 	return localVarReturnValue, localVarHttpResponse, nil
